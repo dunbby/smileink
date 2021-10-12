5d9543f8e9f7a760af1bf418d5eec5242ace0e95
Yordan Yordanov <45233833+dunbby@users.noreply.github.com>
2021-10-12 11:43:29 +0300

    Update header.js

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,6 +1,8 @@
 let world = document.getElementById("world");
+let header = document.getElementById("header");
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
     world.style.top = value * 1 + 'px';
+    header.style.top = value * 0.8 + 'px';
 });
